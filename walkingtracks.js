4b3531f7d8c61fb74855724eaa7561f5bb0ba2e7
Human5280 <human52800@gmail.com>
2023-03-30 23:01:31 +0800

    Update walkingtracks.js

--- walkingtracks.js
+++ walkingtracks.js
@@ -6,9 +6,9 @@ fetch('walkingtracks.json')
   const pathsContainer = document.getElementById('walkingtracks');
 data.paths.forEach(path => {
   const pathElement = document.createElement('div');
-  pathElement.innerHTML = `
+  pathElement.innerHTML = '
     <h2>${path.Title_en}</h2>
     <p><strong>Distance:</strong> ${path.Route_en}</p>
-  ;
+  ;'
   pathsContainer.appendChild(pathElement);
 });
